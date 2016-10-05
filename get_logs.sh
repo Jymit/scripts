d212c460b44e985d99bd886095b1717f0dd229b7
jkhondhu <j_khondhu@hotmail.co.uk>
2016-10-05 08:13:22 +0100

    Create get_logs.sh

---
+++ get_logs.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#CentOS
+#jkhondhu - lrsupport - indexer box 
+
+echo "Creating output temp dir..."
+OUTPUT_DIR=/tmp/$(hostname)-$(date +%F-%H%M%S)
+mkdir -p $OUTPUT_DIR/logs
+
+echo "Copying conf files..."
+cp -r /usr/local/logrhythm/configserver/conf $OUTPUT_DIR
+
+echo "Copying elasticsearch logs..."
+cp /var/log/elasticsearch/* $OUTPUT_DIR/logs
+
+echo "Copying component logs..."
+cp /var/log/persistent/* $OUTPUT_DIR/logs
+
+echo "Creating $OUTPUT_DIR.tgz..."
+tar cfz $OUTPUT_DIR.tgz -C $OUTPUT_DIR .
+
+if [ -f $OUTPUT_DIR.tgz ]; then
+    echo "Deleting $OUTPUT_DIR"
+    rm -rf $OUTPUT_DIR
+fi
